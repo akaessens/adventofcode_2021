613737e1cf7e397416126dd7f5c2522f31affe37
akaessens <24660231+akaessens@users.noreply.github.com>
2021-12-15 11:04:56 +0100

    Day 15

---
+++ 15/part1.py
@@ -0,0 +1,70 @@
+import numpy as np
+import heapq
+
+inputfile = open('input.txt', 'r')
+lines = inputfile.readlines()
+
+xlen = 100
+ylen = 100
+data = np.zeros((ylen, xlen), dtype=np.uint32)
+
+
+for x, line in enumerate(lines):
+    for y, char in enumerate(line):
+        if char == "\n":
+            continue
+        data[x, y] = int(char)
+
+print(data)
+
+
+def get_adjacent_indices(i, j, m, n):
+    adjacent_indices = []
+    if i > 0:
+        adjacent_indices.append((i-1, j))
+    if i+1 < m:
+        adjacent_indices.append((i+1, j))
+    if j > 0:
+        adjacent_indices.append((i, j-1))
+    if j+1 < n:
+        adjacent_indices.append((i, j+1))
+    return adjacent_indices
+
+def heuristic(pos):
+    return 1.5*(xlen+ylen-2-pos[0]-pos[1])
+
+def treeSearch():
+
+    # init
+    startState = ( heuristic((0,0)), 0, 0, 0)
+    fringe = [startState]
+    heapq.heapify(fringe) # faster than sorting manually
+    visited = set()  # set of already visited states to avoid loops
+
+    while (fringe):
+
+        state = heapq.heappop(fringe)  # get next element
+        visited.add(state[2:4])
+
+        if state[2:4] == (ylen-1, xlen-1):
+            return state
+
+        for child in get_adjacent_indices(state[2], state[3], xlen, ylen):
+            if (child[0], child[1]) not in visited:
+                cost = state[1] + data[child[0], child[1]]
+                heur = heuristic(child)
+                new_state = ( heur + cost, cost, child[0], child[1] )
+
+                heapq.heappush(fringe, new_state )
+
+        print("state", state)
+        #print ("fringe ", fringe)
+        print("visited ", len(visited))
+
+    print("Failure, no path found !")
+    return []
+
+
+path = treeSearch()
+
+print(path)
